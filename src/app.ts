dba2cf1e915e2739c6d0cbcfbf9346375b32db87
programarreza <programarreza@gmail.com>
2024-11-02 16:15:12 +0600

    initial setup complete

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "health care server",
+  });
+});
+
+export default app;
