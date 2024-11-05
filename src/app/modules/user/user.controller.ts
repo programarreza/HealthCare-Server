7fee13787606687ee92bf04780bc3d6ba686922a
programarreza <programarreza@gmail.com>
2024-11-05 23:34:15 +0600

    response updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,10 +2,21 @@ import { Request, Response } from "express";
 import { createAdminIntoDB } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
+  try {
+    const result = await createAdminIntoDB(req.body);
 
-  const result = await createAdminIntoDB(req.body);
-
-  res.send(result);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error: error,
+    });
+  }
 };
 
 export { createAdmin };
