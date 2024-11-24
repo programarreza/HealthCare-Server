0373a74978a2ba7148e7ebfdaefef95bf08756fa
programarreza <programarreza@gmail.com>
2024-11-24 19:21:46 +0600

    added to doctor filter system

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
 import sendResponse from "../../../shared/sendResponse";
+import { doctorFilterableFields } from "./doctor.constant";
 import {
   deleteDoctorIntoDB,
   getAllDoctorsFromDB,
@@ -10,7 +12,10 @@ import {
 } from "./doctor.services";
 
 const getAllDoctors = catchAsync(async (req, res) => {
-  const result = await getAllDoctorsFromDB();
+  const filters = pick(req.query, doctorFilterableFields);
+
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await getAllDoctorsFromDB(filters, options);
 
   sendResponse(res, {
     statusCode: StatusCodes.OK,
--- src/app/modules/doctor/doctor.services.ts
+++ src/app/modules/doctor/doctor.services.ts
@@ -1,14 +1,99 @@
-import { UserStatus } from "@prisma/client";
+import { Prisma, UserStatus } from "@prisma/client";
+import { calculatePagination } from "../../../helpers/pagination.helpers";
 import prisma from "../../../shared/prisma";
+import { TPaginationOptions } from "../../interfaces/TPasination";
+import { doctorSearchableFields } from "./doctor.constant";
+import { TDoctorFilterRequest } from "./doctor.interface";
 
-const getAllDoctorsFromDB = async () => {
+const getAllDoctorsFromDB = async (
+  filters: TDoctorFilterRequest,
+  options: TPaginationOptions
+) => {
+  const { page, limit, skip, sortBy, sortOrder } = calculatePagination(
+    options as any
+  );
+
+  const { searchTerm, specialties, ...filterData } = filters;
+  const andConditions: Prisma.DoctorWhereInput[] = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: doctorSearchableFields.map((field) => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  // doctor > doctorSpecialties > specialties -> title
+
+  if (specialties && specialties.length > 0) {
+    andConditions.push({
+      doctorSpecialties: {
+        some: {
+          specialties: {
+            title: {
+              contains: specialties,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    const filterConditions = Object.keys(filterData).map((key) => ({
+      [key]: {
+        equals: (filterData as any)[key],
+      },
+    }));
+    andConditions.push(...filterConditions);
+  }
+
+  andConditions.push({
+    isDeleted: false,
+  });
+
+  const whereConditions: Prisma.DoctorWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
   const result = await prisma.doctor.findMany({
-    where: {
-      isDeleted: false,
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : { averageRating: "desc" },
+    include: {
+      doctorSpecialties: {
+        include: {
+          specialties: true,
+        },
+      },
+      // review: {
+      //   select: {
+      //     rating: true,
+      //   },
+      // },
     },
   });
 
-  return result;
+  const total = await prisma.doctor.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
 };
 
 const getSingleDoctorFromDB = async (id: string) => {
